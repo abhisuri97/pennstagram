7334c666bab40306da7d06f48faf71206d1c3bea
Abhi <suriabhinav1997@gmail.com>
2017-12-11 20:13:45 -0500

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
   acts_as_voter
-  validates :user_name, presence: true, length: { minimum: 4, maximum: 12 }
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 12 }
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
